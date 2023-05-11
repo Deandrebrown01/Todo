f0f79de1890256f1d3a8330b4140791378596cdf
De'Andre Brown <browndeandre08@yahoo.com>
2023-05-11 14:54:34 -0400

    added styles and more

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+let todos = ref([])
+let newTodo = ref('')
+function number () {
+todos.value.push(newTodo.value)
+
+newTodo.value = ''
+}
+function deleteTodo(index){
+todos.value.splice(index, 1)
+
+}
 </script>
 
 <template>
+<h1>My Todo Application</h1>
+<ul>
+<li v-for="(todo,index ) in todos"> 
+{{ index }}
+{{ todo }}  
+<button @click="deleteTodo(index)">delete</button>
+</li>
+</ul>
+<input v-model="newTodo" @keydown.enter="number">
+<button @click="number">Add Todo</button>
+
+
 
 </template>
 
-<style scoped>
-
+<style>
+ body{
+font-family: 'Times New Roman', Times, serif;
+background-color: rgb(171, 207, 233);
+ }
 </style>
